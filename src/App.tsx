9253cd5de4940cb52a16209546b338f1d54b5d2c
aspire-t <zhijianwubu@gmail.com>
2021-10-13 22:34:08 +0800

    feat: bindActionCreators

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { createStore } from './redux'
+import { createStore, bindActionCreators } from './redux'
 
 const INCREMENT = 'ADD'
 const DECREMENT = 'MINUS'
@@ -15,23 +15,40 @@ const reducer = (state = initState, action: any) => {
   }
 }
 
-let initState = { number: 0 }
+// 决定了store中的初始值，如果这里是1，点击add setState的值变为2
+let initState = { number: 1 }
 
+// 创建仓库
 const store = createStore(reducer, initState)
+
+function add() {
+  return { type: 'ADD' }
+}
+
+function minus() {
+  return { type: 'MINUS' }
+}
+
+const actions = { add, minus }
+
+// boundActions用来简化操作的，或者说是用来让代码更清晰的
+const boundActions = bindActionCreators(actions, store.dispatch)
 
 class Counter extends Component {
   public unsubscribe: any
-  public state: any
+  public state: Record<string, any>
   constructor(props: any) {
     super(props)
     this.state = { number: 0 }
   }
 
   componentDidMount() {
+    // 订阅，用unsubscribe 来取消订阅
     this.unsubscribe = store.subscribe(() => this.setState({ number: store.getState().number }))
   }
 
   componentWillUnmount() {
+    // 取消订阅
     this.unsubscribe()
   }
 
@@ -39,12 +56,23 @@ class Counter extends Component {
     return (
       <div>
         <p>{this.state.number}</p>
+        {/* 点击事件用来派发 */}
         <button onClick={() => store.dispatch({ type: 'ADD' })}>+</button>
         <button onClick={() => store.dispatch({ type: 'MINUS' })}>-</button>
         <button onClick={
           () => {
             setTimeout(() => {
               store.dispatch({ type: 'ADD' })
+            }, 1000)
+          }
+        }>1秒后加1</button>
+        <div>boundActions</div>
+        <button onClick={boundActions.add}>+</button>
+        <button onClick={boundActions.minus}>-</button>
+        <button onClick={
+          () => {
+            setTimeout(() => {
+              boundActions.add()
             }, 1000)
           }
         }>1秒后加1</button>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 7584
+    port: 13176,
+    strictPort: false
   },
   plugins: [react()]
 })
